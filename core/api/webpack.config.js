2c35eaa92f3f57d03b7cbd88bf29604328056a38
Jacqueline van Vuuren <73431041+Jacquelinevv0693@users.noreply.github.com>
2022-12-19 05:21:22 +0000

    core/api/webpack.config

--- core/api/webpack.config.js
+++ core/api/webpack.config.js
@@ -1,38 +1,36 @@
-const path = require('path')
-const nodeExternals = require('webpack-node-externals');
-const webpack = require('webpack');
+import { resolve as _resolve } from 'path';
+import nodeExternals from 'webpack-node-externals';
+import { DefinePlugin } from 'webpack';
 
-module.exports = {
-  mode: 'production',
-  entry: path.resolve(__dirname, 'src', 'index.ts'),
-  externals: [
-    nodeExternals({
-      modulesDir: path.resolve(__dirname, './node_modules'),
-    }),
-  ],
-  output: {
-    path: path.resolve(__dirname, 'dist', 'umd'),
-    filename: 'index.js',
-    libraryTarget: 'umd',
-    library: '@baseline-protocol/api',
-    globalObject: 'this'
-  },
-  resolve: {
-    extensions: ['.js', '.ts'],
-  },
-  module: {
-    rules: [
-      {
-        test: /\.ts$/,
-        loader: "ts-loader",
-        exclude: /(node_modules|test)/,
-      },
-    ],
-  },
-  plugins: [
-    new webpack.DefinePlugin({
-      pkg: require('./package.json'),
-      window: {},
-    }),
-  ]
+export const mode = 'production';
+export const entry = _resolve(__dirname, 'src', 'index.ts');
+export const externals = [
+  nodeExternals({
+    modulesDir: _resolve(__dirname, './node_modules'),
+  }),
+];
+export const output = {
+  path: _resolve(__dirname, 'dist', 'umd'),
+  filename: 'index.js',
+  libraryTarget: 'umd',
+  library: '@baseline-protocol/api',
+  globalObject: 'this'
 };
+export const resolve = {
+  extensions: ['.js', '.ts'],
+};
+export const module = {
+  rules: [
+    {
+      test: /\.ts$/,
+      loader: "ts-loader",
+      exclude: /(node_modules|test)/,
+    },
+  ],
+};
+export const plugins = [
+  new DefinePlugin({
+    pkg: require('./package.json'),
+    window: {},
+  }),
+];
